80dd540cdba216547b24de018e89cdef71c7b4b5
Ethan <260215@Hkis.edu.hk>
2022-05-12 08:58:03 +0800

    Initial Commit

---
+++ cardGaurd/cardGaurdApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardGaurdApp.swift
+//  cardGaurd
+//
+//  Created by Ethan Jin on 12/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct cardGaurdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
